c1b90793c0269483c4ecb8bb6efc17aff3333323
= <brewcrew5498@outlook.com>
2018-04-01 23:26:18 -0700

    allow solvers to see piece orientation

--- scripts/Pieces/Piece.ts
+++ scripts/Pieces/Piece.ts
@@ -2,7 +2,7 @@ import { Location } from "../Location";
 import { Orientation } from "../Orientation";
 
 export abstract class Piece {
-  protected orientation: Orientation;
+  public orientation: Orientation;
   public abstract getRelativeIndices(orientation?: Orientation): Location[];
   constructor(orientation?: Orientation) {
     this.orientation = orientation;
