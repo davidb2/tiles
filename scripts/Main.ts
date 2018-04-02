6d17e020df243332c85978d87fc59e7aaa79d266
= <brewcrew5498@outlook.com>
2018-04-01 23:29:28 -0700

    show all solvers at once

--- scripts/Main.ts
+++ scripts/Main.ts
@@ -1,14 +1,21 @@
 import { GridManager } from "./GridManager";
 import { NaiveSolver } from "./Solvers/NaiveSolver";
 import { BFSSolver } from "./Solvers/BFSSolver";
+import { LSolver } from "./Solvers/LSolver";
+import { SpiralSolver } from "./Solvers/SpiralSolver";
 import * as Pro from "../third_party/q.js";
 import * as Ex from "./Exports";
 
 // make Q() global
 Ex.setQ(Pro);
 
-const K = 10;
-const c: HTMLCanvasElement = <HTMLCanvasElement> window.document.getElementById('mainCanvas');
-const gridManager = new GridManager(c, K);
-const solver = new BFSSolver(K);
-gridManager.solve(solver).then(() => { console.log('done!'); });
+const K = 8;
+const bfsSolver = new BFSSolver(K);
+const spiralSolver = new SpiralSolver(K);
+const lSolver = new LSolver(K);
+let idx = 0;
+for (let solver of [bfsSolver, spiralSolver, lSolver]) {
+  const c: HTMLCanvasElement = <HTMLCanvasElement> window.document.getElementById(`canvas-${idx++}`);
+  const gridManager = new GridManager(c, K);
+  gridManager.solve(solver).then(() => { console.log('done!'); });
+}
